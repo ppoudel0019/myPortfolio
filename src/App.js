406034681503a0d2763ec5b1cb1e087bacc85943
ppoudel0019 <ppoudel0019@users.noreply.github.com>
2021-03-29 14:26:22 -0400

    About page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import MyNavbar from "./components/navbar/navbar";
 import MyCarousel from './components/carousel/carousel';
 import About from './pages/about';
 import Title from './components/title';
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
+import Fade from "react-reveal/Fade";
 
 const App=()=> {
   return (
-    <div className="App">
+    <div className="App" style={{position:"relative"}}>
       <MyCarousel/>
       
       <Title/>
       <MyNavbar/>
-      <About />
-
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./images/laptop1.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+     
       
     </div>
   );
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,32 +3,68 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import MyImage from "../images/laptop1.webp"
+import MyImage from "../images/laptop1.webp";
+import Button from "react-bootstrap/Button";
 
 
 const About = () => {
     return (
         <div id='about'>
-             <div className='about'>
-            <h1>About Me</h1>
+            <div className='about'>
+            <h1 className="pt-3 text-center font-details pb-3">About Me</h1>
             <Container>
-                <Row>
-                    <Col xs={12} md={6}>
+                <Row className="pt-3 pb-5 align-items-center">
+                    <Col xs={10} md={6}>
                         <Row className="justify-content-center mb-2 mr-2 ">
                         <Image className="profile justify-content-end" alt="profile" src={MyImage} thumbnail fluid />
                         </Row>
                     </Col>
                     <Col xs={12} md={6}>
-                        My name is
+                        <Row className=" align-items-start p-2 my-details rounded"> 
+                        My name is Pooja Poudel.
                         <br />
-                         ....
+                        <br />
+                        da da da 
                          <br />
-                         ... 
+                         :D :D :D
                          <br />
-                         da da bla bla bla :D :D :D
+                        bla bla bla 
+                        <br />
+                        <br />
+                        <br />
+                        </Row>
+                        <Row className="d-flex justify-content-center flex-wrap">
+                            <div>
+                                <a href="#contact">
+                                    <Button className="m-2" variant="outline-primary">
+                                    Contact
+                                    </Button>
+                                </a>
+                            </div>
+                            <div>
+                                <a href="xyz" target="_blank" rel="noopener noreferrer">
+                                    <Button className="m-2" variant="outline-success">
+                                     My Resume
+                                    </Button>
+                                </a>
+                            </div>
+                            <div>
+                                <a href="xyz" target="_blank" rel="noopener noreferrer">
+                                    <Button className="m-2" variant="outline-dark">
+                                    GitHub
+                                    </Button>
+                                 </a>
+                            </div>
+                            <div>
+                                <a href="xyz" target="_blank" rel="noopener noreferrer">
+                                    <Button className="m-2" variant="outline-info">
+                                    LinkedIn
+                                    </Button>
+                                </a>
+                            </div>
+                        
+                        </Row>
                     </Col>
-
-
                 </Row>
             </Container>
             </div>   
